3c54cbe11c166e975baa8466962dd449cc47e994
Silas Augusto da Cunha <0997s.a.cunha@gmail.com>
2025-04-08 12:37:17 -0300

    adicionando metedo Get ofertas por categoria na pagina diversão - ao inves de usar o metodo .then optei por .subscribe usando rjx e observables

--- src/app/components/ui/pages/diversao/diversao.component.ts
+++ src/app/components/ui/pages/diversao/diversao.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../../../service/ofertas.service';
+import { OfertasInterface } from '../../../interfaces/ofertas.interface';
 
 @Component({
   selector: 'app-diversao',
   standalone: false,
   templateUrl: './diversao.component.html',
-  styleUrl: './diversao.component.scss'
+  styleUrl: './diversao.component.scss',
+  providers: [OfertasService],
 })
-export class DiversaoComponent {
+export class DiversaoComponent implements OnInit {
+  
+  public ofertas!: OfertasInterface[];
 
+  constructor(private ofertasService: OfertasService){}
+
+
+  ngOnInit() {
+    this.ofertasService.getOfertasCategorias('diversao')
+      .subscribe((ofertas: OfertasInterface[]) => {
+        this.ofertas = ofertas;
+        //console.log(this.ofertas);
+      });
+  }
 }
